0b2dc83c30f45d0399df14740f9df23b744d028b
Larsen00 <jonas.larsen103@gmail.com>
2024-03-19 09:47:21 +0100

    same but better

--- Project3/nuke.jl
+++ Project3/nuke.jl
@@ -25,20 +25,20 @@ function constructA(H,K)
 end
 
 # problem 2 
-function solveIP(H, K)
-    h = length(H)
+function solveIP(chd, H, K)
+    n = length(H)
     myModel = Model(Cbc.Optimizer)
     # If your want ot use GLPK instead use:
     #myModel = Model(GLPK.Optimizer)
 
     A = constructA(H,K)
 
-    @variable(myModel, x[1:h], Bin )
-    @variable(myModel, R[1:h] >= 0 )
+    @variable(myModel, x[1:n], Bin )
+    @expression(myModel, R[i=1:n], sum(A[i,j] * x[j] for j=1:n))
 
-    @objective(myModel, Min, sum(x[j] for j=1:h) )
+    @objective(myModel, Min, sum(x[j] for j=1:n) )
 
-    @constraint(myModel, [i=1:h], sum(A[i,j]*x[j] for j=1:h) >= H[i] + 10 )
+    @constraint(myModel, [i=1:n], R[i] >= H[i] + chd )
 
     optimize!(myModel)
 
@@ -53,7 +53,7 @@ end
 
 
 # problem 3
-function problem3(chd, K, H)
+function smooth_channel(chd, K, H)
     m = Model(Cbc.Optimizer)
 
     n = length(H)
@@ -70,7 +70,8 @@ function problem3(chd, K, H)
     @constraint(m, [i=1:n], R[i] >= H[i] + chd)
 
     # Constraints to ensure z[i] captures the absolute deviation
-    @constraint(m, [i=1:n], z[i] == R[i] - (H[i] + chd))
+    @constraint(m, [i=1:n], z[i] <= R[i] - (H[i] + chd))
+    @constraint(m, [i=1:n], z[i] >= -(R[i] - (H[i] + chd)))
 
     optimize!(m)
     
@@ -84,5 +85,44 @@ function problem3(chd, K, H)
 end
 
 
-solveIP(H,K)
-problem3(10, K, H)
+
+
+function without_neighboring_boms(chd, K, H)
+    # Model
+    m = Model(Cbc.Optimizer)
+
+    # length of data points 
+    n = length(H)
+    A = constructA(H, K)
+
+    # Variables
+    @variable(m, x[1:n], Bin)
+    @variable(m, z[1:n] >= 0)  
+    @expression(m, R[i=1:n], sum(A[i,j] * x[j] for j=1:n))
+
+    # Adjust the objective to minimize the sum of z, representing the total deviation
+    @objective(m, Min, sum(z[i] for i=1:n))
+
+    # Constraint to ensure enough dirt is removed, considering the height and channel depth
+    @constraint(m, [i=1:n], R[i] >= H[i] + chd)
+
+    # Constraints to ensure z[i] captures the absolute deviation
+    @constraint(m, [i=1:n], z[i] == R[i] - (H[i] + chd))
+    @constraint(m, [i=2:n], sum(x[i+j] for j=-1:0) <= 1)
+
+    optimize!(m)
+    
+    if termination_status(m) == MOI.OPTIMAL
+        println("without_neighboring_boms")
+        println("Objective value: ", JuMP.objective_value(m))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+    else
+        println("Optimization was not successful. Return code: ", termination_status(m))
+    end
+end
+
+
+solveIP(chd,H,K)
+# smooth_channel(10, K, H)
+# without_neighboring_boms(10, K, H)
