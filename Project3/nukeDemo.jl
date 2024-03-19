85d30baefef523374c8978b0b51658bd26c8cf9a
Larsen00 <jonas.larsen103@gmail.com>
2024-03-19 08:25:05 +0100

    constructA

--- Project3/nukeDemo.jl
+++ Project3/nukeDemo.jl
@@ -1,4 +1,4 @@
-using GLPK, Cbc, JuMP, SparseArrays
+using GLPK, Cbc, JuMP, SparseArrays, Ipopt
 
 H = [
 10
@@ -20,37 +20,34 @@ K = [
 
 
 function constructA(H,K)
-    h = length(H)
-    A = zeros(h,h)
-    for i in 1:h
-        for j in 1:3
-            if (i+j) <= h
-                A[i,i+j-1] = K[j]
+    # Make a function that returns A when given H and K
+    A = zeros(length(H),length(H))
+    l = length(K)
+    KK = [reshape(K[end:-1:2], (1,l-1));; K]
+    for i in 1:length(H)
+        for j in -l+1:l-1
+            if i + j >= 1 && i + j <= length(H)
+                A[i,i+j] = KK[j+l]
             end
         end
     end
-    for i in 1:h
-        for j in 1:i-1
-            A[i,j] = A[j,i]  
-        end    
-    end      
     return A
 end
 
-println(constructA(H,K))
+# println(constructA(H,K))
 
 # A should be structured as follows
-A = [300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0
-     140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0    0.0
-      40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0
-       0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0
-       0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0
-       0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0
-       0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0
-       0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0
-       0.0    0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0
-       0.0    0.0    0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0
-]
+# A = [300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0    0.0    0.0
+#      140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0    0.0
+#       40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0    0.0
+#        0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0    0.0
+#        0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0    0.0
+#        0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0    0.0
+#        0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0    0.0
+#        0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0   40.0
+#        0.0    0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0  140.0
+#        0.0    0.0    0.0    0.0    0.0    0.0    0.0   40.0  140.0  300.0
+# ]
 
 [300.0 140.0 40.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
  140.0 300.0 140.0 40.0 0.0 0.0 0.0 0.0 0.0 0.0;
@@ -92,3 +89,36 @@ function solveIP(H, K)
 end
 
 solveIP(H,K)
+
+function problem3(chd, K, H)
+    m = Model(Cbc.Optimizer)
+
+    n = length(H)
+    A = constructA(H, K)
+
+    @variable(m, x[1:n], Bin)
+    @variable(m, z[1:n] >= 0)  
+    @expression(m, R[i=1:n], sum(A[i,j] * x[j] for j=1:n))
+
+    # Adjust the objective to minimize the sum of z, representing the total deviation
+    @objective(m, Min, sum(z[i] for i=1:n))
+
+    # Constraint to ensure enough dirt is removed, considering the height and channel depth
+    @constraint(m, [i=1:n], R[i] >= H[i] + chd)
+
+    # Constraints to ensure z[i] captures the absolute deviation
+    @constraint(m, [i=1:n], z[i] >= R[i] - (H[i] + chd))
+    @constraint(m, [i=1:n], z[i] >= - (R[i] - (H[i] + chd)))
+
+    optimize!(m)
+    
+    if termination_status(m) == MOI.OPTIMAL
+        println("Objective value: ", JuMP.objective_value(m))
+        println("x = ", JuMP.value.(x))
+        println("R = ", JuMP.value.(R))
+    else
+        println("Optimization was not successful. Return code: ", termination_status(m))
+    end
+end
+
+problem3(10, K, H)
