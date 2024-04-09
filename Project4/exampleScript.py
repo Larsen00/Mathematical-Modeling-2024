275e17fad566f071ab177d3d3716e6d953560745
Larsen00 <jonas.larsen103@gmail.com>
2024-04-09 10:25:16 +0200

    changes loading

--- Project4/exampleScript.py
+++ Project4/exampleScript.py
@@ -10,21 +10,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# dates of images
+dates = ['0317', '0318', '0319', '0326', '0329', '0331']
+
 # Find all image files
-file_name = glob.glob('Project4/processed/17/*natural_color.npy')
-file_name = file_name + glob.glob('Project4/processed/18/*natural_color.npy')
-file_name = file_name + glob.glob('Project4/processed/19/*natural_color.npy')
-file_name = file_name + glob.glob('Project4/processed/26/*natural_color.npy')
-file_name = file_name + glob.glob('Project4/processed/29/*natural_color.npy')
-file_name = file_name + glob.glob('Project4/processed/31/*natural_color.npy')
+file_name = []
+for day in dates:
+    file_name += glob.glob(f'Project4/processed/{day[2:4]}/*natural_color.npy')
 
 # Point to production data
-excel_str = ['Project4/processed/20240317.xlsx',
-             'Project4/processed/20240318.xlsx',
-             'Project4/processed/20240319.xlsx',
-             'Project4/processed/20240326.xlsx',
-             'Project4/processed/20240329.xlsx',
-             'Project4/processed/20240331.xlsx']
+excel_str = [f'Project4/processed/2024{day}.xlsx' for day in dates]
 
 # Load binary mask outlining Denmark
 mask = np.load('Project4/processed/mask.npy')
@@ -45,8 +40,8 @@ for entry in file_name:
     ind = entry.find('202403')
     
     
-    times.append(entry[ind+8:ind+14])
-    timesDay.append(entry[ind+6:ind+8])
+    times.append(entry[ind+8:ind+14])   # gices time of day hhmmss
+    timesDay.append(entry[ind+6:ind+8]) # gives the date
     
     i +=1
     
@@ -56,14 +51,14 @@ times = np.array(times)
 # get target/production values
 Y = []
 for excel_files in excel_str:
-    target = pd.read_excel(excel_files, usecols="B,F")
+    target = pd.read_excel(excel_files, usecols="B,F") # Minutes1DK, SolorPower
     target_times = target['Minutes1DK']
     # Ensure the column is in datetime format
     target_times = pd.to_datetime(target_times)
     # Format the time to HHMMSS and remove colons
     target_times = target_times.dt.strftime('%H%M%S')    
     
-    for entry in times[timesDay==excel_files[-7:-5]]:
+    for entry in times[timesDay==excel_files[-7:-5]]:  # every times where on the same day as excelfile
         # Find rows where 'column_name' equals 'value_to_find'
         condition = target_times == entry[:-2] + '00'
         ind = target_times.index[condition].tolist()
