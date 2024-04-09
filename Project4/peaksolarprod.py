421e9907e2605a2dd10cf37a64da0e25ab9684a1
HarrisNH <114136788+HarrisNH@users.noreply.github.com>
2024-04-09 17:01:11 +0200

    improve peaksolarprod

--- Project4/peaksolarprod.py
+++ Project4/peaksolarprod.py
@@ -15,6 +15,17 @@ from sklearn.model_selection import train_test_split
 
 # dates of images
 dates = ['0317', '0318', '0319', '0326', '0329', '0331']
+# Function to calculate "closeness" to midday
+def calculate_closeness_month(month):
+    # Assuming midday is the peak and values decrease towards 6 AM and 6 PM
+    # Using a simple model where the maximum closeness is at 12 PM
+    # and minimum at 6 AM and 6 PM. This uses a Gaussian distribution concept.
+    return np.exp(-((month - 12)**2) / (2 * (3**2)))  # Standard deviation is 3 hours for a sharper drop
+def calculate_closeness_hour(hour,month): # month used to determine standard deviation - winter = shorter days
+    # Assuming midday is the peak and values decrease towards 6 AM and 6 PM
+    # Using a simple model where the maximum closeness is at 12 PM
+    # and minimum at 6 AM and 6 PM. This uses a Gaussian distribution concept.
+    return np.exp(-((hour - 12)**2) / (2 * ((0.38/calculate_closeness_month(month))**2)))  # Standard deviation is 3 hours for a sharper drop
 
 # Find all image files
 file_name = []
@@ -32,18 +43,41 @@ Xdata = np.zeros((mask.sum(), len(file_name))) # X-variable: The values from the
 times = []
 timesDay = []
 i = 0
+
+#Just used for testing
+closeness_values_hour =1
+closeness_values_month =1
+
 for entry in file_name:
     img = np.load(entry)
-    
-    dummy = img[:,:,0]+img[:,:,1]-2*img[:,:,2]
-    #dummy = dummy*mask
-    Xdata[:,i] = dummy[mask].flatten()
-    
     # Find time information in filename
     ind = entry.find('202403')
-
     times_new = entry[ind+8:ind+14]
     timesDay_new = entry[ind+6:ind+8]
+
+    dummy = (img[:,:,0]+img[:,:,1]-2*img[:,:,2])[mask].flatten()
+
+    #standardize data
+    if np.std(dummy) != 0:
+        dummy = (dummy-np.mean(dummy))/np.std(dummy)
+
+    hour = (int(times_new[:2]))
+    month = int(entry[ind+4:ind+6])*2 ## because then months have same role as time of day, a bit rough estimate
+    closeness_values_hour = calculate_closeness_hour(hour,month)
+    closeness_values_month = calculate_closeness_month(month)
+    #dummy = (img[:,:,0]+img[:,:,1]-2*img[:,:,2])
+    dummy = dummy*closeness_values_hour*closeness_values_month
+    print(times_new,timesDay_new,closeness_values_hour,closeness_values_month)
+    #MSE results on test data of normalizing with hour of day and month of year on one data split:
+    #BTW gausian distribution is with std = 3
+    #No normalization: 95784
+    #Hour normalization: 34791
+    #Month normalization:88340
+    #Month and hour normalization: 22996
+    #Month and hour normalization + ground intensity: 33978
+    Xdata[:,i] = dummy
+    
+
 
     times.append(entry[ind+8:ind+14])   # gices time of day hhmmss
     timesDay.append(entry[ind+6:ind+8]) # gives the date
@@ -52,7 +86,7 @@ for entry in file_name:
     
 timesDay = np.array(timesDay)
 times = np.array(times)
-    
+
 # get target/production values
 Y = []
 for excel_file in excel_str:
@@ -83,12 +117,12 @@ for excel_file in excel_str:
 Y = np.array(Y)
 print('DONE')
 X = Xdata.T
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=1)
 
 
 
 model = Ridge()
 model.fit(X_train,Y_train)
 Y_test_hat = model.predict(X_test)
-print(sum((Y_test_hat-Y_test)**2)/len(Y_test))
+print((np.round(np.mean((Y_test_hat-Y_test)**2))))
 print('DONE')
