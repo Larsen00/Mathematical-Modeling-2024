4f5f01d43612d428e9694da66de0d411e723db20
HarrisNH <114136788+HarrisNH@users.noreply.github.com>
2024-04-30 08:50:13 +0200

    updates

--- Project4/peaksolarprod.py
+++ Project4/peaksolarprod.py
@@ -1,5 +1,6 @@
 
 import openpyxl
+#from flow_ver3 import *
 import glob
 import numpy as np
 import pandas as pd
@@ -9,37 +10,35 @@ from sklearn.linear_model import Ridge, RidgeCV
 from sklearn.model_selection import train_test_split
 
 print("Loading data")
-folder_path = 'Project4/Processedfull'
-files_in_directory = os.listdir(folder_path)
+path = 'Project4/Processedfull'
+files_in_directory = os.listdir(path)
 dates = [file.replace('.xlsx','') for file in files_in_directory if file.endswith('.xlsx')]
 
 def remove_dates(dates, string_list):
-   return [d for d in dates if d not in string_list]
+   return [d for d in dates if d  not in string_list] ####change back return [d for d in dates if d not in string_list]
 
 # remove some dates
-dates_to_remove =['20240317','20240301']
+dates_to_remove =['20240317']
 dates = remove_dates(dates, dates_to_remove)
 
 # Function to calculate "closeness" to midday
-def month_func(month):
-    # Assuming june,july is the peak and values decrease. This uses a Gaussian distribution concept.
-    return 1#np.exp(-((month - 5.5)**2) / (2 * (5.5**2)))  # Standard deviation is 5.5 hours for a sharper drop
 def hour_func(hour): # month used to determine standard deviation - winter = shorter days
     #This uses a Gaussian distribution concept.
-    return np.exp(-((hour - 12)**2) / (((5.1)**2)))  # Standard deviation is 4.5 hours for a sharper drop
+    return np.exp(-((hour - 12)**2) / (((4)**2)))  # Standard deviation is 4.5 hours for a sharper drop
 
 # Find all image files
 file_name = []
 file_name_removed = []
 for day in dates:
-    file_name += glob.glob(f'{folder_path}/{day[6:8]}/*natural_color.npy')
+    file_name += glob.glob(f'{path}/{day[6:8]}/*natural_color.npy')
 for day in dates_to_remove:
-    file_name_removed += glob.glob(f'{folder_path}/{day[6:8]}/*natural_color.npy')
+    file_name_removed += glob.glob(f'{path}/{day[6:8]}/*natural_color.npy')
+
 # Point to production data
-excel_str = [f'{folder_path}/{day}.xlsx' for day in dates]
+excel_str = [f'{path}/{day}.xlsx' for day in dates]
 
 # Load binary mask outlining Denmark
-mask = np.load( folder_path + '/mask.npy')
+mask = np.load( path + '/mask.npy')
 
 # Allocate memory and load image data
 Xdata = np.zeros((mask.sum(), len(file_name))) # X-variable: The values from the pixels in the images
@@ -47,9 +46,6 @@ times = []
 timesDay = []
 i = 0
 
-#Just used for testing
-closeness_values_hour =1
-closeness_values_month =1
 
 for entry in file_name:
     img = np.load(entry)
@@ -59,39 +55,20 @@ for entry in file_name:
     timesDay_new = entry[ind+6:ind+8]
 
     dummy = (img[:,:,0]+img[:,:,1]-2*img[:,:,2])[mask].flatten()
-    #standardize data
-    if np.std(dummy) != 0:                                      ####### NOTE: delete recommended (ivan)
-        dummy = (dummy-np.mean(dummy))/np.std(dummy)
-    # Round to nearest hour
-    if int(times_new[2:4]) > 30:
-        hour = (int(times_new[:2]))+1
-    else: hour = (int(times_new[:2]))
-    month = int(entry[ind+4:ind+6])
+    minutes = float(times_new[2:4])/60
+    hour = float(times_new[:2]) + minutes
     hour_factor= hour_func(hour)
-    month_factor = month_func(month)
-    #dummy = (img[:,:,0]+img[:,:,1]-2*img[:,:,2])
-    dummy = dummy*hour_factor*month_factor
-    
-    #MSE results on test data of normalizing with hour of day and month of year on one data split:
-    #BTW gausian distribution is with std = 3
-    #No normalization: 95784
-    #Hour normalization: 34791
-    #Month normalization:88340
-    #Month and hour normalization: 22996
-    #Month and hour normalization + ground intensity: 33978
+    dummy = dummy*hour_factor
     Xdata[:,i] = dummy
     
-
-
     times.append(entry[ind+8:ind+14])   # gices time of day hhmmss
     timesDay.append(entry[ind+6:ind+8]) # gives the date
-    
     i +=1
     
 timesDay = np.array(timesDay)
 times = np.array(times)
 
-# get target/production values
+
 Y = []
 for excel_file in excel_str:
     target = pd.read_excel(excel_file, usecols="A,F") # Minutes1DK, SolorPower
@@ -115,30 +92,22 @@ for excel_file in excel_str:
             dummy = target['SolarPower'].iloc[ind]
         else:
             dummy = target['SolarPower'].iloc[ind]
-        
-        Y.append(dummy.values)
-    
-Y = np.array(Y)
+        Y.append(dummy.values[0])
 print('DONE')
 X = Xdata.T
-errors=[]
-avg_distances=[]
-alpha_values = np.linspace(10, 3000, 20)  # Example range from very small to large alphas
-# Initialize RidgeCV
-opt_alphas = []
-for i in range(0,5): 
-    model = RidgeCV(alphas=alpha_values, store_cv_values=True)  
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=2*i)
-    model.fit(X_train,Y_train)
-    Y_test_hat = model.predict(X_test)
-    error = ((np.mean((Y_test_hat-Y_test)**2)))
-    avg_dist = np.mean(np.abs(Y_test_hat-Y_test))
-    errors.append(error)
-    avg_distances.append(avg_dist)
-    opt_alphas.append(model.alpha_)
-print(opt_alphas)
-model = Ridge(alpha=2000)
-print(np.mean(errors))
-print(avg_dist)
+alpha_values = np.linspace(1, 100000, 1000)  # Example range from very small to large alphas
+model = RidgeCV(alphas=alpha_values, store_cv_values=True)  
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=2)
+model.fit(X_train,Y_train)
+model.predict(X_test)
+print(model.alpha_)
+model = Ridge(alpha=model.alpha_)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=4)
+model.fit(X_train,Y_train)
+Y_pred = model.predict(X_test)
+error = ((np.mean((Y_pred-Y_test)**2)))
+avg_diff = np.mean(np.abs(Y_pred-Y_test))
 
+print(np.mean(error))
+print(avg_diff)
 model.fit(X,Y)
--- Project4/predictprod.py
+++ Project4/predictprod.py
@@ -1,66 +1,54 @@
 from peaksolarprod import *
-from Project4.flow_ver2 import *
-from ground import extract_groundintensity
+from flow_ver3 import *
 from datetime import datetime
-path = "./Project4/Processedfull"
+from ground import extract_groundintensity
 
-pred_im = []
-target_days = ['0317','0301']
-objects=range(20,40)
+path = "Project4/Processedfull"
+target_days = ['0317']
+objects=range(10,20) #Which frames to predict
 avg_distances = []
 errors = []
-earth_image = extract_groundintensity()
+pred_im = []
+earth_im = extract_groundintensity()
 for i in range(len(target_days)):
-        V, timesDay, times, mask = load_images(target_days[i], path)
+        excel_data = pd.read_excel(f'Project4/Processedfull/2024{target_days[i]}.xlsx', usecols="A,F") 
+        target_times = pd.to_datetime(excel_data['Minutes1UTC']).dt.strftime('%H%M%S')
+        V, timesDay, times_predicted, mask = load_images(target_days[i], path)
 
         # Define how many objects subject to calculating optical flow
         dps_images = Lucas_Kanade_method(V, objects=objects)
 
         # plot_with_noise_filtering(dps_images, V, timesDay, times, mask, show=False)
         # interpolate_flow(dps_images, V, timesDay, times, mask, objects=objects, show=True)
-        pred_im,timestamp = extrapolate_flow(dps_images, V, earth_image,timesDay, times, mask, minutes_after=15, objects=objects, show=False)
+        pred_ims,timestamp = extrapolate_flow(dps_images, V,timesDay, times_predicted, mask, minutes_after=15, objects=objects, show=False)
        # Assuming pred_im is already defined as per your description
-        reshaped_arrays = [arr.reshape(-1, 1) for arr in pred_im]  # Reshape each array to (57*156, 1)
-        final_array = np.hstack(reshaped_arrays)  # Stack them horizontally
+        pred_ims = np.hstack([arr.reshape(-1, 1) for arr in pred_ims])  # Reshape each array to (57*156, 1)
+  
         # Apply the mask to each column
         mask = mask.flatten()
-        masked_data = final_array[mask, :]
+        masked_ims = pred_ims[mask, :]
+        y_true = []
+        for i, frame_index in enumerate(objects):
+            time = times_predicted[frame_index]
+            minutes = float(time[2:4])/60
+            hour = float(time[0:2])+minutes
+            hour_factor = hour_func(hour)
+            masked_ims[:,i] = masked_ims[:,i]*hour_factor
 
-        #if np.std(final_array,axis=0) != 0:
-        masked_data = (masked_data-np.mean(dummy,axis=0))/np.std(masked_data,axis=0)
-
-        #Write code to calculate month and hour of predicted image. 
-        #month_factor = month_func(month_new)
-      
-
-        #fix timestamp
-            # Format the timestamp properly
-        timestamp = datetime.strptime(timestamp, '%Y%m%d_%H%M%S').strftime('%H%M%S')
-        timestamp = timestamp[:4] + '00'
-        if int(timestamp[2:4]) > 30:
-            hour_new = int(timestamp[0:2])+1
-        else: hour_new = int(timestamp[0:2])
-        hour_factor = hour_func(hour_new)
-        futureimage = masked_data*month_factor*hour_factor
-        y_pred = model.predict(futureimage.T)
+            timestamp = time[:4]+'00'
+            # Correct filtering approach using Pandas
+            condition = target_times == timestamp
+            matching_indices = target_times[condition].index.tolist()
+            if matching_indices:
+                matching_row = excel_data.iloc[matching_indices]
+                y_true.append(matching_row['SolarPower'].values[0])
+        
+        y_pred = model.predict(masked_ims.T)
 
         #Get correct production values
         #excelfile = f'{timestamp}.xlsx'
-        excel_data = pd.read_excel(excel_file, usecols="A,F") 
-
-        target_times = pd.to_datetime(excel_data['Minutes1UTC']).dt.strftime('%H%M%S')
-
-        # Correct filtering approach using Pandas
-        condition = target_times == timestamp
-        matching_indices = target_times[condition].index.tolist()
-        if matching_indices:
-            matching_row = excel_data.iloc[matching_indices]
-            y_true = matching_row['SolarPower'].values
-        
-
-        error = ((np.mean((y_true-y_pred)**2)))
-        avg_dist = np.mean(np.abs(y_true-y_pred))
-        errors.append(error)
-        avg_distances.append(avg_dist)
-print(errors)
-print(avg_distances)
+        errors.append(((np.mean((y_true-y_pred)**2))))
+        avg_distances.append(np.mean((y_true-y_pred)))
+print(np.mean(errors))
+print(np.mean(avg_distances))
+print('end')
