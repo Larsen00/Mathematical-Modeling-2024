d79eff34ed5eb079540d795e011cb461c60bdee9
Ivan Fan <ivan.fan.dk@gmail.com>
2024-05-03 10:09:37 +0200

    minor fix on plot.py

--- Project4/plot.py
+++ Project4/plot.py
@@ -14,8 +14,8 @@ y_prediction_list_interpolation = np.load('y_prediction_list_interpolation.npy',
 
 test_date = "20240331"
 x = [datetime.strptime(test_date + t, "%Y%m%d%H%M%S") for t in true_time_list]
-plt.plot(x, y_prediction_list, label='prediction')
-plt.plot(x, y_prediction_list_interpolation, label='prediction with interpolation')
+plt.plot(x, y_prediction_list, label='Prediction')
+plt.plot(x, y_prediction_list_interpolation, label='Prediction with interpolation')
 plt.plot(x, y_true_list, label='true values')
 plt.legend()
 plt.title('Solar production prediction')
@@ -27,8 +27,8 @@ plt.clf()
 
 # print(np.mean(errors))
 # print(np.mean(errors_interpolation))
-plt.plot(x, errors, label='without interplation')
-plt.plot(x, errors_interpolation, label='with interpolation')
+plt.plot(x, errors, label='Prediction without interplation')
+plt.plot(x, errors_interpolation, label='Prediction with interpolation')
 print(f"MSE Error: {np.mean(errors)}")
 print(f"MSE Error with interpolation: {np.mean(errors_interpolation)}")
 plt.legend()
@@ -40,8 +40,8 @@ plt.tight_layout()
 plt.savefig('line4.svg')
 plt.clf()
 
-plt.plot(x, average_distances,'o', label='without interpolation')
-plt.plot(x, average_distances_interpolation, 'o',label='with interpolation')
+plt.plot(x, average_distances,'o', label='Prediction without interpolation')
+plt.plot(x, average_distances_interpolation, 'o',label='Prediction with interpolation')
 print(f"Average Distance: {np.mean(average_distances)}")
 print(f"Average Distance with interpolation: {np.mean(average_distances_interpolation)}")
 plt.axhline(y=0.0, color='r', linestyle='--')
@@ -54,8 +54,8 @@ plt.savefig('line5.svg')
 plt.clf()
 
 x = [datetime.strptime(test_date + t, "%Y%m%d%H%M%S") for t in true_time_list]
-plt.plot(y_true_list, y_prediction_list, 'o', label='prediction')
-plt.plot(y_true_list, y_prediction_list_interpolation, 'o', label='prediction with interpolation')
+plt.plot(y_true_list, y_prediction_list, 'o', label='Prediction without interpolation')
+plt.plot(y_true_list, y_prediction_list_interpolation, 'o', label='Prediction with interpolation')
 a = np.arange(500)
 plt.plot(a, a, label='y=x')
 plt.xlabel('True values')
